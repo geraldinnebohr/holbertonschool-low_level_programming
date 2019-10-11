0e75afbae37c4cb2efd38e109f0c78f12d445e8a
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-11 22:14:42 +0000

    [FIX] type index

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned int index = key_index((unsigned char *)key, ht->size);
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *pocket = ht->array[index];
 
 	if (pocket == NULL || key == NULL)
