7509790e1ec5b7ba80abf6a2db4ef932fd82a8a3
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-11 16:04:48 +0000

    [FIX] if malloc worked condition added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,10 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashtable;
 
 	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	hashtable->size = size;
 
-	if (hashtable == NULL)
-		return (NULL);
 	return (hashtable);
 }
