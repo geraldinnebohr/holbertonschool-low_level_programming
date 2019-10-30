6919b4ca4e6a34401d9f232feeaa6444c33d5d8f
geraldinnebohr <geraldinnebohr@gmail.com>
2019-10-30 20:11:02 +0000

    [FIX] added condition

--- 0x1B-sorting_algorithms/100-shell_sort.c
+++ 0x1B-sorting_algorithms/100-shell_sort.c
@@ -8,21 +8,21 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	int i, j, gap;
 	int tmp;
 
 	gap = 1;
+	tmp = 0;
 
-	while (gap < size)
+	while (gap < (int)size)
 		gap = (gap * 3) + 1;
 	gap = (gap - 1) / 3;
 
 	while (gap > 0)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < (int)size; i++)
 		{
-			tmp = 0;
-			for (j = i; array[j] < array[j - gap]; j -= gap)
+			for (j = i; (j - gap) >= 0 && array[j] < array[j - gap]; j -= gap)
 			{
 				tmp = array[j];
 				array[j] = array[j - gap];
